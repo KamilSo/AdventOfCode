a94691ce40c79725755b785dd11e8cede6edb96e
KamilSo <k.sobolewski2@gmail.com>
2022-12-05 20:47:15 +0000

    Day 5 Finished

--- Day 5/Day 5 Part 2.py
+++ Day 5/Day 5 Part 2.py
@@ -1,12 +1,12 @@
 input = open('input.txt','rt')
 input = input.readlines()
-'''stacks = input[:8]
+stacks = input[:8]
 stack_number = input[8:9]
-moves = input[10:]'''
+moves = input[10:]
 stack_index=[]
-stacks = input[:3]
+'''stacks = input[:3]
 stack_number = input[3:4]
-moves = input[5:]
+moves = input[5:]'''
 
 for stack_nr in stack_number:
     for n in range(len(stack_nr)):
@@ -35,27 +35,31 @@ for index_nr in stack_index:
     stack_name = 'stack' + str(stack_nr[index_nr])
     globals()[str(stack_name)].reverse()
 
-for x in range(10): #this is just to check if code before works (It does work) xD
-    try:
-        stack_name = 'stack' + str(x)
-        print(globals()[str(stack_name)])
-    except:
-        pass
 
 for move in moves: #something probably wrong below here
+    crates_to_move = []
     try:
         for n in range(int(move[5])):
             stack_name = 'stack' + str(move[12])
             crate_to_move = globals()[stack_name].pop(-1)
             stack_name = 'stack' + str(move[17])
-            globals()[stack_name].append(crate_to_move)
+            crates_to_move.append(crate_to_move)
     except:
         x = move[5:7]
         for n in range(int(x)):
             stack_name = 'stack' + str(move[13])
             crate_to_move = globals()[stack_name].pop(-1)
             stack_name = 'stack' + str(move[18])
-            globals()[stack_name].append(crate_to_move)
+            crates_to_move.append(crate_to_move)
+    crates_to_move.reverse()
+    for crate in crates_to_move:
+        try:
+            stack_name = 'stack' + str(move[17])
+            globals()[stack_name].append(crate)
+        except:
+            stack_name = 'stack' + str(move[18])
+            globals()[stack_name].append(crate)
+
 
 for n in range(10):
     try:
