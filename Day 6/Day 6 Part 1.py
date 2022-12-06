7edd90fa36ed246830b1796e54e6b52ce5175f77
KamilSo <k.sobolewski2@gmail.com>
2022-12-06 14:22:11 +0000

    Day 5 Finished

---
+++ Day 6/Day 6 Part 1.py
@@ -0,0 +1,30 @@
+string = open('input.txt','rt')
+lines = string.readlines()
+All_lines = ''
+
+for line in lines:
+    All_lines += str(line)
+
+def check_duplicate(list):
+    if len(list) == len(set(list)):
+        return False
+    else:
+        return True
+
+def marker(string):
+    stop = False
+    while not stop:
+        for n in range(len(string)):
+            packet_range = []
+            for i in range(14):
+                packet_range.append((string[i+n]))
+            duplicate = check_duplicate(packet_range)
+            if duplicate == True:
+                pass
+            else:
+                stop = True
+                print(n+14)
+                break
+
+
+marker(All_lines)
