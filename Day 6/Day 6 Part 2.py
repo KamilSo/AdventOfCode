16f4b0c06b7da72fc58e37457c4888e32d35575e
KamilSo <k.sobolewski2@gmail.com>
2022-12-06 14:23:38 +0000

    Day 6 Finished

--- Day 6/Day 6 Part 2.py
+++ Day 6/Day 6 Part 2.py
@@ -16,14 +16,14 @@ def marker(string):
     while not stop:
         for n in range(len(string)):
             packet_range = []
-            for i in range(4):
+            for i in range(14):
                 packet_range.append((string[i+n]))
             duplicate = check_duplicate(packet_range)
             if duplicate == True:
                 pass
             else:
                 stop = True
-                print(n+4)
+                print(n+14)
                 break
 
 
